5564308262a430875ebf4d021600079021d3422c
jaydeaton <jayrdeaton@gmail.com>
2019-02-08 13:30:38 -0600

    added batch and duplicate

--- src/adapters/APIAdapter.js
+++ src/adapters/APIAdapter.js
@@ -13,6 +13,31 @@ let APIAdapter = class APIAdapter {
     if (data.headers) this.headers = data.headers;
     if (data.encodingMethod) this.encodingMethod = data.encodingMethod;
   };
+  async batch(schema, query, body) {
+    let endpoint = schema.resourceName || schema.name;
+    let data = JSON.stringify(wholeObject(body));
+    let request = {
+      url: `${this.domain}/${endpoint}/batch`,
+      method: 'PUT',
+      headers: this.headers,
+      data
+    };
+    if (query) {
+      request.params = {};
+      Object.keys(query).forEach((key) => {
+        if (typeof query[key] === 'object') {
+          if (this.encodingMethod) {
+            request.params[key] = this.encodingMethod(query[key]);
+          } else {
+            request.params[key] = JSON.stringify(query[key]);
+          };
+        } else { request.params[key] = query[key] };
+      });
+    };
+    if (process.env.NODE_ENV === 'EMPORIUM_TEST') throw request;
+    let response = await axios(request);
+    return response.data;
+  };
   async count(schema, query) {
     let endpoint = schema.resourceName || schema.name;
     let request = {
