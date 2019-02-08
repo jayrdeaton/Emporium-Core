b8ccbff592e8e8277a482edc24573868b5101158
jaydeaton <jayrdeaton@gmail.com>
2019-02-08 13:31:34 -0600

    added batch and duplicate to memory adapter

--- src/adapters/MemoryAdapter.js
+++ src/adapters/MemoryAdapter.js
@@ -11,6 +11,14 @@ let MemoryAdapter = class MemoryAdapter {
     objects = arrayQuery(objects, query);
     return objects.length;
   };
+  async batch(schema, query, body) {
+    let endpoint = schema.resourceName || schema.name;
+    let objects = [];
+    if (this.database[endpoint]) Object.assign(objects, this.database[endpoint]);
+    objects = arrayQuery(objects, query);
+    for (let object of objects) for (let key of Object.keys(body)) object[key] = body[key];
+    return null;
+  };
   async create(schema, body) {
     let endpoint = schema.resourceName || schema.name;
     let objects = await this.get(schema, null);
@@ -33,6 +41,17 @@ let MemoryAdapter = class MemoryAdapter {
     };
     this.database[endpoint] = objects;
     return null;
+  };
+  async duplicate(schema, identifier) {
+    // This is broken because it doesnt generate a new identifier
+    if (!schema.identifier) return null;
+    let endpoint = schema.resourceName || schema.name;
+    let query = {filter: {[schema.identifier]: identifier}};
+    let objects = await this.get(schema, query);
+    let object = null;
+    if (objects.length > 0) object = objects[0];
+    if (object) objects.push(new Object(object));
+    return object;
   };
   async find(schema, identifier) {
     if (!schema.identifier) return null;
