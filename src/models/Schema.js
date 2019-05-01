fc58a8cc9b193cb9b97381970861d08691763b8a
jaydeaton <jayrdeaton@gmail.com>
2019-04-30 19:34:15 -0500

    restructure

--- src/models/Schema.js
+++ src/models/Schema.js
@@ -1,18 +1,22 @@
 module.exports = class Schema {
-  constructor(data) {
-    if (!data) data = {};
-    this.attributes = data;
-    this.hidden = [];
-    this.locked = [];
-    this.discarded = [];
-    this.required = [];
-    this.name = null;
-    this.resourceName = null;
-    this.strict = true;
-    this.identifier = null;
-    this.adapter = null;
-    for (let key of Object.keys(data)) {
-      let value = data[key]
+  constructor(attributes, options) {
+    // options
+    if (!options) options = {};
+    const { adapter, discarded, hidden, identifier, locked, name, required, resourceName, strict } = options;
+    this.adapter = adapter || null;
+    this.discarded = discarded || [];
+    this.hidden = hidden || [];
+    this.identifier = identifier || null;
+    this.locked = locked || [];
+    this.name = name || null;
+    this.required = required || [];
+    this.resourceName = resourceName || null;
+    this.strict = strict || true;
+    // attributes
+    if (!attributes) attributes = {};
+    this.attributes = attributes;
+    for (const key of Object.keys(attributes)) {
+      const value = attributes[key]
       if (typeof value === 'object') {
         if (!value.type) throw new Error(`Schema attribute '${key}' requires a type`);
         if (value.hidden) this.hidden.push(key);
