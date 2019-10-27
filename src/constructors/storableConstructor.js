499f18f434c1763f6003f229bc2c07aa7bfe6c92
jaydeaton <jayrdeaton@gmail.com>
2019-10-27 12:20:33 -0500

    before and after storage fix

--- src/constructors/storableConstructor.js
+++ src/constructors/storableConstructor.js
@@ -88,14 +88,19 @@ module.exports = (emporium, schema) => {
       body = this.removeDiscardedAttributes(body);
       for (let key of schema.required) if (updateKeys.includes(key)) if (typeof body[key] === 'undefined' || body[key] === null) throw new Error(`${schema.name} missing required value: ${key}!`);
       for (const key of Object.keys(body)) if (!updateKeys.includes(key)) delete body[key];
+      if (schema.beforeStorage) schema.beforeStorage(body);
       let result = await schema.adapter.update(schema, body, query);
+      if (schema.afterStorage) schema.afterStorage(body);
       return result ? this.convertObjects(result) : null;
     };
     async save(query) {
+      // return this.constructor.update(this, query)
       if (!schema.writable) throw new Error(`${schema.name} is not writable`);
       Storable.removeDiscardedAttributes(this);
       for (let key of schema.required) if (typeof this[key] === 'undefined' || this[key] === null) throw new Error(`${schema.name} missing required value: ${key}!`);
-      let object = await schema.adapter.update(schema, this, query);
+      if (schema.beforeStorage) schema.beforeStorage(this);
+      const object = await schema.adapter.update(schema, this, query);
+      if (schema.afterStorage) schema.afterStorage(this);
       return new this.constructor(object);
     };
     delete() {
